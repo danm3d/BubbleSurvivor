7af6e90ae33ab2bb8ed80b21e06fb165bdccd74b
Daniel Mendelowitz <chappiez@live.co.za>
2015-03-10 09:54:24 +0200

    Added Google Play Games Services
    Only signin currently works

---
+++ Assets/Scripts/SignIn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+public class SignIn : MonoBehaviour
+{
+	public void SignInUser()
+	{
+		if (!PlayGamesPlatform.Instance.localUser.authenticated)
+		{
+			PlayGamesPlatform.Instance.localUser.Authenticate((bool success) =>
+			{
+				if (success)
+				{
+					Debug.Log("We're signed in! Welcome " + PlayGamesPlatform.Instance.localUser.userName);
+					// We could start our game now
+				}
+				else
+				{
+					Debug.Log("Oh... we're not signed in.");
+				}
+			});
+		}
+		else
+		{
+			Debug.Log("You're already signed in.");
+			// We could also start our game now
+		}
+	}
+}
