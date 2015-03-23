819e38a17248e199ff5f507b4195ed36322aa760
Daniel Mendelowitz <chappiez@live.co.za>
2015-03-23 15:21:51 +0200

    Made some tweaks to accomodate maze mode
    Added "neutral" bubble colour for maze mode.
    Updated maze map using curve editors

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,108 +5,110 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
 
-    private PlayerController playerControl;
-    private HighscoreManager scoreManager;
-    public Text scoreText, qualityLabel;
-    public Toggle accel, sounds, shake;
-    public GameObject quitMenu, mainMenu, optionsMenu, scoreMenu, social;
+	private PlayerController playerControl;
+	private HighscoreManager scoreManager;
+	public Text scoreText, qualityLabel;
+	public Toggle accel, sounds, shake;
+	public GameObject quitMenu, mainMenu, optionsMenu, scoreMenu, social;
 
-    public bool Accelerometer
-    {
-        get
-        {
-            return playerControl.CurrentGameSettings.useAccelerometer;
-        }
-        set
-        {
-            playerControl.CurrentGameSettings.useAccelerometer = value;
-        }
-    }
+	public bool Accelerometer
+	{
+		get
+		{
+			return playerControl.CurrentGameSettings.useAccelerometer;
+		}
+		set
+		{
+			playerControl.CurrentGameSettings.useAccelerometer = value;
+		}
+	}
 
-    public bool Sounds
-    {
-        get
-        {
-            return playerControl.CurrentGameSettings.sounds;
-        }
-        set
-        {
-            playerControl.CurrentGameSettings.sounds = value;
-            Camera.main.GetComponent<AudioSource>().enabled = value;
-        }
-    }
+	public bool Sounds
+	{
+		get
+		{
+			return playerControl.CurrentGameSettings.sounds;
+		}
+		set
+		{
+			playerControl.CurrentGameSettings.sounds = value;
+			Camera.main.GetComponent<AudioSource>().enabled = value;
+		}
+	}
 
-    public bool Vibes
-    {
-        get
-        {
-            return playerControl.CurrentGameSettings.vibes;
-        }
-        set
-        {
-            playerControl.CurrentGameSettings.vibes = value;
-        }
-    }   
+	public bool Vibes
+	{
+		get
+		{
+			return playerControl.CurrentGameSettings.vibes;
+		}
+		set
+		{
+			playerControl.CurrentGameSettings.vibes = value;
+		}
+	}
 
-    void Start()
-    {
-        playerControl = GetComponent<PlayerController>();
-        scoreManager = GetComponent<HighscoreManager>();
-        social.GetComponent<GooglePlay_Social>().SignInUser();
-    }
-	
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (optionsMenu.activeSelf)
-            {
-                optionsMenu.SetActive(false);
-                mainMenu.SetActive(true);
-            } else if (scoreMenu.activeSelf)
-            {
-                scoreMenu.SetActive(false);
-                mainMenu.SetActive(true);
-            } else
-                quitMenu.SetActive(true);
-        }
-    }
+	void Start()
+	{
+		playerControl = GetComponent<PlayerController>();
+		scoreManager = GetComponent<HighscoreManager>();
+		social.GetComponent<GooglePlay_Social>().SignInUser();
+	}
 
-    public void DisplayHighscore()
-    {
-        scoreText.text = scoreManager.GetScores();
-    }
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (optionsMenu.activeSelf)
+			{
+				optionsMenu.SetActive(false);
+				mainMenu.SetActive(true);
+			}
+			else if (scoreMenu.activeSelf)
+			{
+				scoreMenu.SetActive(false);
+				mainMenu.SetActive(true);
+			}
+			else
+				quitMenu.SetActive(true);
+		}
+	}
 
-    public void Options()
-    {
-        accel.isOn = Accelerometer;
-        sounds.isOn = Sounds;
-        shake.isOn = Vibes;
-        qualityLabel.text = QualitySettings.names [QualitySettings.GetQualityLevel()];
-    }
+	public void DisplayHighscore()
+	{
+		scoreText.text = scoreManager.GetScores();
+	}
 
-    public void Play()
-    {
-        Application.LoadLevel(1);
-    }
+	public void Options()
+	{
+		accel.isOn = Accelerometer;
+		sounds.isOn = Sounds;
+		shake.isOn = Vibes;
+		qualityLabel.text = QualitySettings.names[QualitySettings.GetQualityLevel()];
+	}
 
-    public void Quit()
-    {
-        Application.Quit();
-    }
+	public void Play(int level)
+	{
+		Application.LoadLevel(level);
+	}
 
-    public void IncreaseQuality()
-    {
-        QualitySettings.IncreaseLevel();
-        qualityLabel.text = QualitySettings.names [QualitySettings.GetQualityLevel()];
-        playerControl.CurrentGameSettings.qualityLevel = QualitySettings.GetQualityLevel();
-    }
+	public void Quit()
+	{
+		Application.Quit();
+	}
 
-    public void DecreaseQuality()
-    {
-        QualitySettings.DecreaseLevel();
-        qualityLabel.text = QualitySettings.names [QualitySettings.GetQualityLevel()];
-        playerControl.CurrentGameSettings.qualityLevel = QualitySettings.GetQualityLevel();
-    }
+	public void IncreaseQuality()
+	{
+		QualitySettings.IncreaseLevel();
+		qualityLabel.text = QualitySettings.names[QualitySettings.GetQualityLevel()];
+		playerControl.CurrentGameSettings.qualityLevel = QualitySettings.GetQualityLevel();
+	}
+
+	public void DecreaseQuality()
+	{
+		QualitySettings.DecreaseLevel();
+		qualityLabel.text = QualitySettings.names[QualitySettings.GetQualityLevel()];
+		playerControl.CurrentGameSettings.qualityLevel = QualitySettings.GetQualityLevel();
+	}
 
 }
